36707fdda42618aab997de16d63dade6403a85d9
Patrick Carter <xCarter93@gmail.com>
2025-02-02 08:23:42 -0500

    updated clerk middleware to handle signout properly

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,9 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
-	if (!isPublicRoute(request)) {
-		await auth.protect();
+	const { userId, redirectToSignIn } = await auth();
+	if (!isPublicRoute(request) || !userId) {
+		return redirectToSignIn();
 	}
 });
 
