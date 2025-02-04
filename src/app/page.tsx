937c175cef51e2da0eccdd328c1314d282d9e886
Patrick Carter <xCarter93@gmail.com>
2025-02-03 22:16:05 -0500

    changed the veritcal spacing on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ import { BetaBanner } from "@/components/beta-banner";
 
 export default function LandingPage() {
 	return (
-		<div>
-			{/* Beta Banner */}
-			<BetaBanner />
-
-			{/* Hero Section */}
-			<DarkGridHero />
+		<div className="flex flex-col gap-12">
+			{/* Beta Banner and Hero Section Wrapper */}
+			<div className="flex flex-col">
+				<BetaBanner />
+				<DarkGridHero />
+			</div>
 
 			{/* Features Section */}
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -97,7 +97,7 @@ export default function LandingPage() {
 			<SlidePricing />
 
 			{/* Footer */}
-			<footer>
+			<footer className="mt-auto">
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
 					<div className="flex flex-col items-center space-y-2">
 						<p>© 2024 EventPulse. All rights reserved.</p>
